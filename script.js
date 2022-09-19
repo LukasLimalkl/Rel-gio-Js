6d98a7f367fc0b87771960f4027acae917440729
LukasLimalkl <lukinhaslima063@gmail.com>
2022-09-19 19:10:36 -0300

    Second

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const clock = setInterval(function time (){
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
 
+    if(hr < 10 ) hr = '0' + hr
+    if(min < 10 ) min = '0' + min
+    if(sec < 10 ) sec = '0' + sec
+    
+    
     hours.textContent = hr;
     minutes.textContent = min;
     seconds.textContent = sec;
